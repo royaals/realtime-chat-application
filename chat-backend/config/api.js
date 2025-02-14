2ace48032f8069b9021ef99bed7a4f6e51f2be5b
Royal <royal.21ai040@sode-edu.in>
2025-02-14 20:30:05 +0530

    Removed locked folder

---
+++ chat-backend/config/api.js
@@ -0,0 +1,13 @@
+module.exports = {
+  rest: {
+    defaultLimit: 25,
+    maxLimit: 100,
+    withCount: true,
+    jjw: {
+      stringify: {
+        type: 'space',
+        space: '  ',
+      },
+    },
+  },
+};
