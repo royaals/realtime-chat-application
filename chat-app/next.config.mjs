ebabd066e8649c3ecacdccefecb8fadf1f1686ff
Royal <royal.21ai040@sode-edu.in>
2025-02-15 14:32:39 +0530

    remove build errors

--- chat-app/next.config.mjs
+++ chat-app/next.config.mjs
@@ -1,3 +1,4 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -10,5 +11,7 @@ const nextConfig = {
   // Add this for Vercel deployment
   output: 'standalone',
 }
-
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 module.exports = nextConfig
